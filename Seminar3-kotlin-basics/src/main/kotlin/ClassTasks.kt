5d1778048df7d7c524f842ebe0aeba5699d87635
Alex Zabelin <alexandre.zabelin@gmail.com>
2023-11-23 00:16:54 +0300

    feat: complete all tasks

--- Seminar3-kotlin-basics/src/main/kotlin/ClassTasks.kt
+++ Seminar3-kotlin-basics/src/main/kotlin/ClassTasks.kt
@@ -1,3 +1,6 @@
+import kotlin.math.PI
+import kotlin.math.pow
+
 /**
  * Задание 1: Объявите классы Circle и Square – наследников базового (Point).
  * Реализуйте readonly свойство, вычисляющее площадь каждой фигуры, а также функцию display()
@@ -6,22 +9,34 @@
  * (3 балла)
  */
 
-class Point(val X: Double, val Y: Double) {
+open class Point(private val x: Double, private val y: Double) {
+    constructor(x: Int, y: Int) : this(x.toDouble(), y.toDouble())
 
-    val area: Double = 0.0
+    open val area get() = 0.0
 
     fun display() {
-        // Формат вывода – S = ... , X = ... , Y = ...
+        println("S = ${this.area}, X = $x, Y = $y")
     }
 }
+
+class Circle(x: Double, y: Double, private val r: Double) : Point(x, y) {
+    constructor(x: Int, y: Int, r: Int) : this(x.toDouble(), y.toDouble(), r.toDouble())
+    override val area get() = PI * r.pow(2)
+}
+
+class Square(x: Double, y: Double, private val l: Double) : Point(x, y) {
+    constructor(x: Int, y: Int, l: Int) : this(x.toDouble(), y.toDouble(), l.toDouble())
+    override val area get() = l.pow(2)
+}
+
 
 // Используйте эту функцию для запуска кода
 // Раскомментируйте нужные участки в процессе реализации
 fun main() {
-//    val p = Point(34, 56)
-//    val c = Circle(46, 34, 7)
-//    val s = Square(46, 34, 8)
-//    p.display()
-//    c.display()
-//    s.display()
+    val p = Point(34, 56)
+    val c = Circle(46, 34, 7)
+    val s = Square(46, 34, 8)
+    p.display()
+    c.display()
+    s.display()
 }
--- Seminar3-kotlin-basics/src/main/kotlin/CollectionsTasks.kt
+++ Seminar3-kotlin-basics/src/main/kotlin/CollectionsTasks.kt
@@ -1,10 +1,10 @@
+fun isUnique(array: List<String>, elem: String): Boolean = array.indexOf(elem) == array.lastIndexOf(elem)
+
 /**
  *  Задание 1: Заменить все повторяющиеся элементы входного списка на "blahblah".
  *  (1 балл)
  */
-fun replaceElements(array: List<String>): List<String> {
-    return listOfNotNull()
-}
+fun replaceElements(array: List<String>) = array.map{ if (isUnique(array, it)) it else "blahblah" }
 
 
 /**
@@ -12,16 +12,18 @@ fun replaceElements(array: List<String>): List<String> {
  *  Слова в строке обязательно разделяются пробелом.
  *  (1 балл)
  */
-fun uniqueWords(text: String): Int {
-    return 0
+fun uniqueWords(text: String): Int  {
+    val array = text.split(" ")
+    val predicate: (String) -> Boolean = {isUnique(array, it)}
+
+    return array.count(predicate)
 }
 
 // Используйте эту функцию для запуска кода
 // Раскомментируйте нужные участки в процессе реализации
 fun main() {
-    //val text = ""
-    //println(uniqueWords(text))
+    val text = "abc cde abc abc eft xyz"
+    println(uniqueWords(text))
 
-    // Вызвать для text и вывести результат замены на экран
-    //replaceElements()
+    println(replaceElements(text.split(" ")))
 }
